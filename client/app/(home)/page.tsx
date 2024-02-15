f91c79bdd45876ae4ea18a54d0841681f8fa4a3d
Fadel-AlMasri <masrifadel0@gmail.com>
2024-02-15 11:25:36 +0200

    phase1

---
+++ client/app/(home)/page.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { playfair } from "@/public/Fonts/fonts";
+import { Close, BurgerMenu, Language } from "@/components/icons";
+const page = () => {
+  return (
+    <div>
+      page
+      <h1>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem
+        quis quasi et consectetur possimus dolores, fugit at? Veritatis esse
+        nisi animi laboriosam, eum velit, pariatur molestias, illum dignissimos
+        suscipit obcaecati.
+      </h1>
+      <h1 className="">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem
+        quis quasi et consectetur possimus dolores, fugit at? Veritatis esse
+        nisi animi laboriosam, eum velit, pariatur molestias, illum dignissimos
+        suscipit obcaecati.
+      </h1>
+    </div>
+  );
+};
+
+export default page;
---
+++ client/tailwind.config.js
@@ -0,0 +1,28 @@
+const { nextui } = require("@nextui-org/react");
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        neutral: {
+          50: "#FFFFFF",
+          100: "#B8B8B8",
+          200: "#373737",
+          300: "181818",
+          400: "#000000",
+        },
+      },
+    },
+  },
+  // darkMode: 'class',
+  plugins: [nextui()],
+};
